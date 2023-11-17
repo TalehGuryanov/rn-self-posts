6a463d6d367af94298462628300a64913db31144
Taleh Guryanov <taleh.gurjanov@gmail.com>
2023-11-17 21:12:49 +0400

    added TabNavigator

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,40 +1,81 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {MainScreen} from "../screens/MainScreen";
 import {AboutScreen} from "../screens/AboutScreen";
 import {BookedScreen} from "../screens/BookedScreen";
 import {PostScreen} from "../screens/PostScreen";
 import {THEME} from "../theme";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-const Stack = createStackNavigator();
+const HomeStack = createNativeStackNavigator();
+const PostsStack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function PostsStackScreen ( ) {
+  return (
+      <PostsStack.Navigator initialRouteName="Booked Stack" screenOptions={headerStyle}>
+        <PostsStack.Screen
+            name="Booked Stack"
+            component={BookedScreen}
+            options={{title: "Booked"}}
+        />
+        <PostsStack.Screen
+            name="Post"
+            component={PostScreen}
+            options={({ route }) => ({ title: ` Post ${route.params.postId}` })}
+        />
+      </PostsStack.Navigator>
+  );
+}
+
+function  HomeStackScreen ( ) {
+  return (
+      <HomeStack.Navigator initialRouteName="Main" screenOptions={headerStyle}>
+        <HomeStack.Screen
+            initialRouteName="Main"
+            name="Main"
+            component={MainScreen}
+            options={{title: "Main"}}
+        />
+        <HomeStack.Screen
+            name="Post"
+            component={PostScreen}
+            options={({ route }) => ({ title: ` Post ${route.params.postId}` })}
+        />
+      </HomeStack.Navigator>
+  );
+}
+
+function  TabNavigator ( ) {
+  return (
+      <Tab.Navigator screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({color}) => {
+          let iconName;
+  
+          if (route.name === 'Home') {
+            iconName = 'ios-albums';
+          } else if (route.name === 'Booked') {
+            iconName = 'ios-star';
+          }
+          return <Ionicons name={iconName} size={25} color={color} />;
+        },
+        tabBarActiveTintColor: THEME.MAIN_COLOR,
+        })}
+        initialRouteName="Home"
+      >
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Booked" component={PostsStackScreen} />
+      </Tab.Navigator>
+  );
+}
 
 
 export const AppNavigation = () => {
   return  (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Main" screenOptions={headerStyle}>
-          <Stack.Screen
-              initialRouteName="Main"
-              name="Main"
-              component={MainScreen}
-              options={{title: "Main"}}
-          />
-          <Stack.Screen
-              name="About"
-              component={AboutScreen}
-              options={{title: "About"}}
-          />
-          <Stack.Screen
-              name="Booked"
-              component={BookedScreen}
-              options={{title: "Booked"}}
-          />
-          <Stack.Screen
-              name="Post"
-              component={PostScreen}
-              options={({ route }) => ({ title: ` Post ${route.params.postId}` })}
-          />
-        </Stack.Navigator>
+        <TabNavigator />
       </NavigationContainer>
   )
 }
--- src/screens/BookedScreen.js
+++ src/screens/BookedScreen.js
@@ -1,19 +1,38 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
+import {DATA} from "../data";
+import {Post} from "../components/Post";
+import React from "react";
+import {HeaderButtons, Item} from "react-navigation-header-buttons";
+import {AppHeaderIcon} from "../components/AppHeaderIcon";
+import { useLayoutEffect } from 'react';
 
-export const BookedScreen = () => {
+export const BookedScreen = ({navigation}) => {
+  const openPostHandler = post => {
+    navigation.navigate('Post', {postId: post.id})
+  }
+  
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+          <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+            <Item title="Toggle drawer" iconName="ios-menu" onPress={() => console.log('menu')} style={{marginHorizontal: 0}}/>
+          </HeaderButtons>),
+    });
+  }, [])
+  
   return (
-      <View style={styles.center}>
-        <Text>
-          Booked Screen
-        </Text>
+      <View style={styles.wrapper}>
+        <FlatList
+            data={DATA.filter(post => post.booked)}
+            keyExtractor={item => item.id.toString()}
+            renderItem={({item}) => <Post post={item} onOpen={openPostHandler}/>}
+        />
       </View>
   )
 }
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  wrapper: {
+    padding: 10,
   }
 });
