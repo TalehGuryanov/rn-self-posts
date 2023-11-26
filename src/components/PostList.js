a274887f5bda3058a1bdd3a26d962e0aa401585b
Taleh Guryanov <taleh.gurjanov@gmail.com>
2023-11-26 20:32:32 +0400

    removed mock data

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,14 @@
-import { View, StyleSheet, FlatList } from 'react-native';
+import {View, StyleSheet, FlatList, Text} from 'react-native';
 import {Post} from "./Post";
 
 export const PostList = ({data, onOpen}) => {
+  if(!data.length) {
+    return <View style={styles.wrapper}>
+      <Text style={styles.noItems}>
+        No posts yet
+      </Text>
+    </View>
+  }
   return (
       <View style={styles.wrapper}>
         <FlatList
@@ -16,5 +23,11 @@ export const PostList = ({data, onOpen}) => {
 const styles = StyleSheet.create({
   wrapper: {
     padding: 10,
+  },
+  noItems: {
+    fontFamily: 'open-sans-regular',
+    textAlign: 'center',
+    marginVertical: 10,
+    fontSize: 18,
   }
 });
